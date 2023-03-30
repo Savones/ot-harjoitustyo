8ca4f53c539569d77affbbc289e5b1151f360ef6
Essi Savonius <essi.savonius@hotmail.fi>
2023-03-30 22:31:24 +0300

    muutos ui ruudun kokoon

--- memomy_game/src/ui/game_ui.py
+++ memomy_game/src/ui/game_ui.py
@@ -1,21 +1,21 @@
 import pygame
-HEIGHT = 500
-WIDTH = 500
+HEIGHT = 600
+WIDTH = 600
 
 def draw_screen():
     pygame.init()
     display = pygame.display.set_mode((WIDTH, HEIGHT))
     square_color = (255, 255, 255)
 
-    x = 135
-    y = 75
+    x = 155
+    y = 100
     for i in range(1, 10):
-        pygame.draw.rect(display, square_color, pygame.Rect(x, y, 70, 70))
+        pygame.draw.rect(display, square_color, pygame.Rect(x, y, 90, 90))
         if i % 3 == 0:
-            y += 80
-            x = 135
+            y += 100
+            x = 155
         else:
-            x += 80
+            x += 100
 
     pygame.display.flip()
 
